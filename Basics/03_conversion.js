36d3934e73319b63691b823904b8a26d4949c9fb
deepanshuBaniya <161374351+deepanshuBaniya@users.noreply.github.com>
2024-04-07 13:29:07 +0000

    coversion config done

---
+++ Basics/03_conversion.js
@@ -0,0 +1,15 @@
+let score = 42
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
+let number = 1
+let number1 = ""
+let booleannumber = Boolean(number)
+console.log(booleannumber)
+let booleannumber1 = Boolean(number1)
+console.log(booleannumber1)
+
+// 1 = true 
+// "" = false 
