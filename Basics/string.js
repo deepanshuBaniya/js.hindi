ffac6da736459fcbbbd2dd536828dd415ab5c26a
deepanshuBaniya <161374351+deepanshuBaniya@users.noreply.github.com>
2024-04-07 17:33:46 +0000

    string config done

---
+++ Basics/string.js
@@ -0,0 +1,13 @@
+const name ="deepanshu"
+const repoCount = 79
+// this is old version 
+//console.log( "hii my name is " + name + " and my repo count is" + repoCount )
+
+// this is ne version to write a string it si called Strig interpolation
+console.log( `hii my anme is ${name} nad my repo count is ${repoCount} `)
+
+
+const user =  new String("deepanshu kumar")
+console.log(user.length)
+console.log(user.toUpperCase())
+console.log(user.)
