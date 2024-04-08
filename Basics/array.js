dfb749131305a7718b8f3096f7e55ff5c424a381
deepanshuBaniya <161374351+deepanshuBaniya@users.noreply.github.com>
2024-04-08 10:36:23 +0000

    simaple array is config

---
+++ Basics/array.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,5,6]
+console.log(arr)
+
+// add the element in the array
+arr.push(8)
+console.log(arr)
+
+// remove the element inn array
+arr.pop()
+console.log(arr)
+
+//this is use  to change array element into string
+const newarr = arr.join()
+console.log(newarr)
+console.log(typeof newarr)
