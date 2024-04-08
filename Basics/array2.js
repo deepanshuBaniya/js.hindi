9499a1ef6bcd2d672d330179eb401aad3bdada6d
deepanshuBaniya <161374351+deepanshuBaniya@users.noreply.github.com>
2024-04-08 10:48:41 +0000

    arraay config

---
+++ Basics/array2.js
@@ -0,0 +1,10 @@
+const indian = ["gagan" , "deepak" , "rohan"]
+const south = ["deep" , "raj" ,  "virat"]
+
+// add two arrays 
+const all = indian.concat(south)
+//console.log(all)
+
+// moreden method to add more than one array
+const allhero = [...indian, ...south]
+//console.log(allhero)
